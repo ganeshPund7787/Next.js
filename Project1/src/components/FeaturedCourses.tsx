c3789b4ef8585fb17d5fbb445080e2bfe2d8f3a1
ganeshPund7787 <ganupund7787@gmail.com>
2024-09-15 03:13:11 -0700

    Add Feature Cources section

---
+++ Project1/src/components/FeaturedCourses.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Link from "next/link";
+import courseData from "../data/music_cources.json";
+import { BackgroundGradient } from "./ui/background-gradient";
+import MoreBtn from "./MoreBtn";
+
+interface Course {
+  id?: number;
+  title?: string;
+  slug?: string;
+  description?: string;
+  price?: number;
+  instructor?: string;
+  isFeatured?: boolean;
+  image?: string;
+}
+
+function FeaturedCourses() {
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+
+  return (
+    <div className="py-12 min-h-screen bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="text-base text-cyan-400 font-semibold tracking-wide uppercase">
+            FEATURED COURSES
+          </h2>
+          <p className="my-5 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            Learn With the Best
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 mx-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-teal-600 mt-4 mb-2 dark:text-cyan-400">
+                    {course.title}
+                  </p>
+                  <img src={course.image} className="my-3" alt={course.image} />
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link className="mt-2" href={`/courses/${course.slug}`}>
+                    <MoreBtn text={" Learn More"} />
+                  </Link>
+                </div>
+              </BackgroundGradient>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-20 text-center">
+        <Link href={"/courses"}>
+          <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
+            View All courses
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedCourses;
--- Project1/src/components/HeroSection.tsx
+++ Project1/src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         fill="white"
       />
       <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+        <h1 className="mt-20 text-teal-600 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master the art of music
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
