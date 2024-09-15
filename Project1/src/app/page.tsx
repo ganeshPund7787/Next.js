11b4cd20cb31a4068d1e2eac647c401826fa3320
ganeshPund7787 <ganupund7787@gmail.com>
2024-09-14 22:51:29 -0700

    Intial commit

---
+++ Project1/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-center text-2xl">Hello</h1>
+    </main>
+  );
+}
